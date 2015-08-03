f606d14bbd1639ef5dba99397fe0ea7a64f72b34
Jesus Tiendeo <jesus@iMac-de-Jesus.local>
2015-08-03 10:16:12 +0200

    Initial Commit

---
+++ CustomVideo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CustomVideo
+//
+//  Created by Jesus Tiendeo on 3/8/15.
+//  Copyright (c) 2015 Jesus Tiendeo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ CustomVideo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CustomVideo
+//
+//  Created by Jesus Tiendeo on 3/8/15.
+//  Copyright (c) 2015 Jesus Tiendeo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
